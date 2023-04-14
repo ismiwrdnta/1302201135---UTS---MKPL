55b41aacf4d8c2a2a2ef6c6d72b9a9db6f2f2e7c
Ismi Wardanita <90543101+ismiwrdnta@users.noreply.github.com>
2023-04-14 22:43:13 +0700

    mengubah class employee dan menambahkan class baru

---
+++ src/lib/person.java
@@ -0,0 +1,72 @@
+package lib;
+
+public class person {
+    private enum Jgender{
+		Pria,
+		Wanita
+	}
+
+    private String employeeId;
+	private String firstName;
+	private String lastName;
+	private String idNumber;
+	private String address;
+
+    private boolean isForeigner;
+	private Jgender gender;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getIdNumber() {
+        return idNumber;
+    }
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public boolean isForeigner() {
+        return isForeigner;
+    }
+    public void setForeigner(boolean isForeigner) {
+        this.isForeigner = isForeigner;
+    }
+    public Jgender getGender() {
+        return gender;
+    }
+    public void setGender(Jgender gender) {
+        this.gender = gender;
+    }
+
+    public person(String employeeId, String firstName, String lastName, String idNumber, String address, boolean isForeigner, Jgender gender) {
+		this.employeeId = employeeId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = idNumber;
+		this.address = address;
+		this.isForeigner = isForeigner;
+		this.gender = gender;	
+	}
+    public person() {
+    }
+}
